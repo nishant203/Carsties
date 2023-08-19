8c809183f686a9bdeb71c20aa22c98143ff96ae0
Nishant Varshney <nishant203@gmail.com>
2023-08-19 19:15:13 +0530

    Frontend changes

---
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { AiOutlineCar } from "react-icons/ai";
+import Search from "./Search";
+import Logo from "./Logo";
+export default function Navbar() {
+  return (
+    <header
+      className="
+      sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md
+    "
+    >
+      <div>
+        <Logo />
+      </div>
+      <div>
+        <Search />
+      </div>
+      <div>Login</div>
+    </header>
+  );
+}
